4bc4417ee41e1fe5fe320bfc1601c8f5ed3a4bf3
chisom life eke <chisomlifeeke@gmail.com>
2025-10-23 12:05:41 +0100

    new code TypeCasting

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,6 +2,15 @@ public class TypeCasting{
 	public static void main(String[] args){
 		//widenig 
 		double num =10;
-		System.out.printf("The value is %f ",num);
-	}
+		System.out.printf("The value is %f \n",num);
+		//narrowing
+		double price = 32.89;
+		int result =(int)price;
+		System.out.printf("the value is  %d ",result); 
+		
+		
+		}
 }
+
+// write down all the 57 eyword and some definitins in one sentense be side it
+// save it in a msdocx then push the code 
