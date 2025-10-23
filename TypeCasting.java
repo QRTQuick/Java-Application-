f495130bc531d4f38b4edc91b6fea0765afe110a
chisom life eke <chisomlifeeke@gmail.com>
2025-10-23 11:45:07 +0100

    update

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,6 +2,6 @@ public class TypeCasting{
 	public static void main(String[] args){
 		//widenig 
 		double num =10;
-		Sytem.out.printf("The value is %f ",num);
+		System.out.printf("The value is %f ",num);
 	}
 }
