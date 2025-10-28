2cd2b74c297638a05a907e951d022243bcec25e9
chisom life eke <chisomlifeeke@gmail.com>
2025-10-28 10:23:34 +0100

    operators

---
+++ OperatorPart1.java
@@ -0,0 +1,22 @@
+public class OperatorPart1{
+	public static void main(String[] argrs){
+		//assingment operators are used to assign variable eg equal sign "="
+		//compund assingment operators shorthand way to update variables eg below
+		int num = 50;
+		num += 8;
+		System.out.println(num);
+		
+		num -= 7;
+		System.out.println(num);
+		
+		num *= 10;
+		System.out.println(num);
+		
+		num /= 5;
+		System.out.println(num);
+		
+		num %= 3;
+		System.out.println(num);
+		
+	}
+}
