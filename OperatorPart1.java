efb181cefc894eb41d102e2df568a0707fe97792
chisom life eke <chisomlifeeke@gmail.com>
2025-10-28 10:28:53 +0100

    operators

--- OperatorPart1.java
+++ OperatorPart1.java
@@ -1,6 +1,7 @@
 public class OperatorPart1{
 	public static void main(String[] argrs){
 		//assingment operators are used to assign variable eg equal sign "="
+		
 		//compund assingment operators shorthand way to update variables eg below
 		int num = 50;
 		num += 8;
@@ -18,5 +19,13 @@ public class OperatorPart1{
 		num %= 3;
 		System.out.println(num);
 		
+		
+		// aritimetic operators
+		
+		int num1 = 10;
+		int num2 = 4;
+		int sum = num1 + num2;
+		
+		System.out.printf("the sum of %d and %d is %d",num1,num2,sum);
 	}
 }
